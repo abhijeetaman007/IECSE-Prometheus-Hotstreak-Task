6d453e42c0b26dbca935c469ef4e92a9a1d9ba1c
abhijeetaman007 <53662741+abhijeetaman007@users.noreply.github.com>
2020-04-29 05:07:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mysqlConnection= require("./connection");
 const QuestionsRoutes =require("./routes/questions");
-
+const AddquestionRoute =require("./routes/addquestion");
 var app = express();
 app.use(bodyParser.json());
 
@@ -10,6 +10,6 @@ app.get("/",(req,res) => {
 res.send("Hello World!!")
 })
 app.use("/questions",QuestionsRoutes);
-
+app.use("/addquestion",AddquestionRoute);
 
 app.listen(3000);
