f479015f8b8149215c0347a2600065eb5dbb82be
abhijeetaman007 <53662741+abhijeetaman007@users.noreply.github.com>
2020-04-29 05:06:28 +0530

    Update addquestion.js

--- routes/addquestion.js
+++ routes/addquestion.js
@@ -5,7 +5,7 @@ const mysqlConnection= require("../connection");
 Router.post('/',(req,res)=>{
     console.log(req.body);
 
-    var sql="INSERT INTO questions(q_id,title,body,input_format,output_format,constraints,s_input,o_output,tc1,tc2,tc3,ans1,ans2,ans3,score) VALUES("+req.body.q_id+",'"+req.body.title+"','"+req.body.body+"','"+req.body.input_format+"','"+req.body.output_format+"','"+req.body.constraints+"','"+req.body.s_input+"','"+req.body.o_output+"','"+req.body.tc1+"','"+req.body.tc2+"','"+req.body.tc3+"','"+req.body.ans1+"','"+req.body.ans2+"','"+req.body.ans3+"',"+req.body.score+")"
+    var sql="INSERT INTO questions(q_id,title,body,input_format,output_format,constraints,s_input,o_output,tc1,tc2,tc3,ans1,ans2,ans3,score) VALUES(null,"+req.body.q_id+",'"+req.body.title+"','"+req.body.body+"','"+req.body.input_format+"','"+req.body.output_format+"','"+req.body.constraints+"','"+req.body.s_input+"','"+req.body.o_output+"','"+req.body.tc1+"','"+req.body.tc2+"','"+req.body.tc3+"','"+req.body.ans1+"','"+req.body.ans2+"','"+req.body.ans3+"',"+req.body.score+")"
     mysqlConnection.query(sql,(err,rows,fields)=>{
         if(err) throw err
         res.send("Saved Successfully");
